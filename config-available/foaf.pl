71992f802adf53458a961b56b12bcba118907cca
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-09 11:42:18 +0100

    Initial version

---
+++ config-available/foaf.pl
@@ -0,0 +1,13 @@
+:- module(conf_foaf, []).
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf_library)).
+
+/** <module> Provide FOAF schema and namespace
+
+This module provides the FOAF schema and   the  prefix =foaf= for use in
+Prolog.
+*/
+
+:- rdf_register_ns(foaf, 'http://xmlns.com/foaf/0.1/').
+:- rdf_attach_library(foaf(rdf)).
+:- rdf_load_library(foaf).
